fce431a0d2b0adc56833d33c46479fe9b0cc328a
283018 <283018@student.pwr.edu.pl>
2025-05-12 15:32:42 +0200

    correct build message

--- breach_solvers/brute/breach_solver_back/__init__.py
+++ breach_solvers/brute/breach_solver_back/__init__.py
@@ -17,10 +17,18 @@ build_dir = os.path.join(package_dir, "build", build_subdir)
 module_name = "breach_solver_cpp"
 module_path = None
 
-for file in os.listdir(build_dir):
-    if file.startswith(module_name) and (file.endswith(".pyd") or file.endswith(".so") or file.endswith(".dylib")):
-        module_path = os.path.join(build_dir, file)
-        break
+try:
+    for file in os.listdir(build_dir):
+        if file.startswith(module_name) and (file.endswith(".pyd") or file.endswith(".so") or file.endswith(".dylib")):
+            module_path = os.path.join(build_dir, file)
+            break
+except FileNotFoundError:
+    raise ImportError(
+        "Build directory not found. Please build the module first:\n"
+        r"'python .\breach_solvers\brute\breach_solver_back\setup.py build clean --all'"
+        "&&"
+        "python3 ./breach_solvers/brute/breach_solver_back/setup.py build clean --all"
+    )
 
 if module_path is None:
     if not os.path.exists(build_dir):
