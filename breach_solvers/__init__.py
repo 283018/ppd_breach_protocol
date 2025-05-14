6c0c1af0950f3a86be85a5e1c3294a6987b3fa41
283018 <283018@student.pwr.edu.pl>
2025-05-14 16:11:12 +0200

    updated gurobi import

--- breach_solvers/__init__.py
+++ breach_solvers/__init__.py
@@ -3,6 +3,7 @@
 from .solvers_protocol import get_solver
 from .gurobi.solver_gurobi import GurobiSolver
 from .brute.solver_brute import BruteSolver
+from .ant_colony.solver_ac import AntColSolver
 
 
 __all__ = ['get_solver']
