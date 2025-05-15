03bf49d38301201d6763cdb4ef6780acf7abbf5f
283018 <283018@student.pwr.edu.pl>
2025-05-15 23:56:51 +0200

    Updated non-optimal solution handling for gurobi

--- breach_solvers/gurobi/solver_gurobi.py
+++ breach_solvers/gurobi/solver_gurobi.py
@@ -1,5 +1,5 @@
-from breach_solvers.solvers_protocol import Solver, register_solver
-from core import Task, Solution
+from breach_solvers.solvers_protocol import Solver, register_solver, OptimizationError
+from core import Task, Solution, DUMMY_TASK, NoSolution
 
 from numpy import int8, array, zeros, dot
 from time import perf_counter
@@ -23,13 +23,43 @@ import gurobipy as gp
 # TODO: separate method + init log
 @register_solver('gurobi')
 class GurobiSolver(Solver):
-    _allowed_kwargs = {}
+    _allowed_kwargs = {'output_flag': bool, 'strict_opt': bool}
+
+    _initialized:bool = False
+    mode: gp.Model
+
+    def _warm_up(self):
+        from os import devnull
+        import sys
+
+        original_stdout = sys.stdout
+        sys.stdout = open(devnull, 'w')
+        self.model = gp.Model('BreachProtocol')
+        sys.stdout = original_stdout
+
+        self.model.setParam('OutputFlag', 0)
+
+        try:
+            self._initialized = True
+
+            start_init = perf_counter()
+            self.__call__(DUMMY_TASK)
+            end_init = perf_counter()
+        except Exception as e:
+            self._initialized = False
+            raise RuntimeError(f"Error while initialization gurobi solver occurred: {e}") from e
+        else:
+            print(f"\rSuccessfully initialized gurobi solver in {end_init - start_init:.4} sec", flush=True)
 
     # pycharm cant reed types from dataclasses and struggling with gurobi :/
-    # noinspection PyTypeChecker
+    # noinspection PyTypeChecker, PyUnresolvedReferences
     def __call__(self, task: Task, **kwargs):
         """Solve breach protol task using linear programming solver Gurobi via gurobipy API."""
         self._validate_kwargs(kwargs)
+        output_flag = kwargs.get('output_flag', 0)
+        strict_opt = kwargs.get('strict_opt', False)
+        if not self._initialized:
+            self._warm_up()
 
         matrix = task.matrix
         buffer_size = int(task.buffer_size) # to avoid problems with numpy.int
@@ -42,50 +72,50 @@ class GurobiSolver(Solver):
         max_points = costs.sum()
         unused_cell_reward = 0.1 * (max_points / d_amo)
 
-
         # setup
-        model = gp.Model("BreachProtocol")
-        model.setParam('OutputFlag', 0)
+        # env = gp.Env()
+        # env.setParam('LogToConsole', 0)
+        # model = gp.Model("BreachProtocol")
+        self.model.setParam('OutputFlag', output_flag)
 
         # Do mierzenia czasu pomijamy przekształcanie danych i inicjalizacje skupiamy się przede wszystkim na budowaniu modelu
         # (w naszym przypadku model w pełni zależy od inputu i nie można go używać kilkukrotnie) i jego optymalizacji
         time_start = perf_counter()
 
         # wybór drogi
-        x = model.addVars(n, n, buffer_size, vtype=gp.GRB.BINARY, name='x')
+        x = self.model.addVars(n, n, buffer_size, vtype=gp.GRB.BINARY, name='x')
 
         # co najwyżej jedna komórka wybierana w każdym kroku
         for t in range(buffer_size):
-            model.addConstr(x.sum('*', '*', t) <= 1, name=f'one_cell_per_step_{t}')
+            self.model.addConstr(x.sum('*', '*', t) <= 1, name=f'one_cell_per_step_{t}')
 
         # ciągłość drogi - nie można wybrać komórki po niewybranej komórki (ale można nie wybierać na końcu)
         for t in range(1, buffer_size):
             prev = x.sum('*', '*', t - 1)
             curr = x.sum('*', '*', t)
-            model.addConstr(curr <= prev, name=f'continuous_path_at_{t}')
+            self.model.addConstr(curr <= prev, name=f'continuous_path_at_{t}')
 
         # komórka może być wybrana co najwyżej raz
         for i in range(n):
             for j in range(n):
-                model.addConstr(x.sum(i, j, '*') <= 1, name=f'cell_{i}_{j}_once')
+                self.model.addConstr(x.sum(i, j, '*') <= 1, name=f'cell_{i}_{j}_once')
 
         # maksymalna ilość kroków
         used_buffer = x.sum('*', '*', '*')
-        model.addConstr((used_buffer <= buffer_size), name='max_steps')
+        self.model.addConstr((used_buffer <= buffer_size), name='max_steps')
 
         # rozpoczęcie zawsze w pierwszym rzędzie - dla wszystkich rzędów poza pierwszym w kroku 0 nie można wybierać komórki
-        model.addConstrs((x.sum(i, '*', 0) == 0 for i in range(1, n)), name='start_in_first_row')
+        self.model.addConstrs((x.sum(i, '*', 0) == 0 for i in range(1, n)), name='start_in_first_row')
 
         # poruszanie się po macierzy zgodnie z zasadami rząd/kolumna
-        # TODO: indicators?
         for t in range(1, buffer_size):
             for i in range(n):
                 for j in range(n):
                     if t % 2 == 1:
                         # dla dowolnej komórki w kroku t musi być wybrana dowolna komórka w tej samej *kolumnie* w kroku t-1
-                        model.addConstr(x[i, j, t] <= x.sum('*', j, t - 1), name=f'move_rule_col_{i}_{j}_step_{t}')
+                        self.model.addConstr(x[i, j, t] <= x.sum('*', j, t - 1), name=f'move_rule_col_{i}_{j}_step_{t}')
                     else:
-                        model.addConstr(x[i, j, t] <= x.sum(i, '*', t - 1), name=f'move_rule_row_{i}_{j}_step_{t}')
+                        self.model.addConstr(x[i, j, t] <= x.sum(i, '*', t - 1), name=f'move_rule_row_{i}_{j}_step_{t}')
 
         # # bezpośrednio sekwencja buffera
         buffer_seq = [
@@ -107,14 +137,14 @@ class GurobiSolver(Solver):
             z_i = {}
 
             for p in range(valid_p):                            # ∀ valid starting pos
-                z_i[p] = model.addVar(
+                z_i[p] = self.model.addVar(
                     vtype=gp.GRB.BINARY, name=f'z_{i}_{p}')
             z.append(z_i)
 
             for p in range(valid_p):
                 for s in range(curr_len):                       # ∀ symbolu demona
                     t = p + s
-                    model.addGenConstrIndicator(
+                    self.model.addGenConstrIndicator(
                         z_i[p],                         # jeśli dla tego demona dla tej pozycji startowej is_valid
                         1,                              # == 1 (True)
                         buffer_seq[t] == demons[i][s],  # to w buffer (na odpowiedniej pozycji) MUSI być symbol demona
@@ -122,7 +152,7 @@ class GurobiSolver(Solver):
         
 
         # y variables for demon activation
-        y = model.addVars(d_amo, vtype=gp.GRB.BINARY, name='y')
+        y = self.model.addVars(d_amo, vtype=gp.GRB.BINARY, name='y')
 
         # jeśli chociaż jedna z starting_pos valid to y[i] == 1
         for i in range(d_amo):
@@ -130,14 +160,14 @@ class GurobiSolver(Solver):
 
             # sekwencja demona może występować w buffer kilkakrotnie np. demon=[1, 3], buffer = [1, 3, 1, 3]
             # y[i] == 0 jeśli *żadna* z z[i][p] nie == 1
-            model.addConstr(y[i] <= gp.quicksum(z[i][p] for p in range(valid_p)), name=f'active_y_{i}_at_least_one')
+            self.model.addConstr(y[i] <= gp.quicksum(z[i][p] for p in range(valid_p)), name=f'active_y_{i}_at_least_one')
             # jeśli dowolna z z[i][p] == 1, y[i] też == 1
             for p in range(valid_p):
-                model.addConstr(y[i] >= z[i][p], name=f'active_y_{i}_for_{p}')
+                self.model.addConstr(y[i] >= z[i][p], name=f'active_y_{i}_for_{p}')
 
 
         # maksymalizacja punktów (obv) + wynagrodzenie za niezużycie buffera
-        model.setObjective(
+        self.model.setObjective(
             gp.quicksum(
                 costs[i] * y[i] for i in range(d_amo)) +
                 unused_cell_reward * (buffer_size - used_buffer),
@@ -148,13 +178,18 @@ class GurobiSolver(Solver):
         # Tf is wrong with you, Gurobi, why exceptions are protected and not referenced..?
         # noinspection PyUnresolvedReferences,PyProtectedMember
         try:
-            model.optimize()
+            self.model.optimize()
         except gp._exception.GurobiError as e:
-            print("Model is too large to solve: \n    {}".format(e))
-            return Solution(path=array([[-1, -1]])).fill_solution(task), -1
+            if strict_opt:
+                raise OptimizationError("Gurobi optimization failed:\n    {}".format(e)) from e
+            else:
+                return NoSolution(reason=e), -1
 
-        if model.status != gp.GRB.OPTIMAL:
-            print(f"Solution status is not optimal: model.status={model.status}")
+        if self.model.status != gp.GRB.OPTIMAL:
+            if strict_opt:
+                raise OptimizationError("Gurobi solution status is not optimal:\n    {}".format(gp.GRB.OPTIMAL))
+            else:
+                print(f"Solution status is not optimal: model.status={self.model.status}")
 
         time_end = perf_counter()
 
@@ -174,38 +209,6 @@ class GurobiSolver(Solver):
         # ic('')
         # ic(task)
         # ic(x_path, buffer_nums, y_active, y_total_points)
-
+        self.model.reset()
         return Solution(x_path, buffer_nums, y_active, y_total_points), time_end - time_start
 
-
-
-# if __name__ == '__main__':
-#     from icecream import ic
-#     # from core import verify_solution, solution_print
-#     buffer_size1 = 8
-#     demons1 = (
-#         array([1, 2]),
-#         array([3, 4]),
-#         array([5, 1, 2])
-#     )
-#     d_costs1 = array([1, 2, 3])
-#     matrix1 = array([
-#         [3, 1, 5, 5, 3, 5, 2],
-#         [5, 6, 2, 2, 5, 5, 1],
-#         [5, 5, 4, 2, 6, 5, 2],
-#         [1, 2, 1, 3, 2, 2, 1],
-#         [1, 5, 2, 4, 6, 6, 4],
-#         [3, 1, 3, 5, 5, 2, 3],
-#         [3, 5, 1, 5, 6, 3, 2],
-#     ])
-#     gb_solver = SolverGurobi()
-#     task1 = Task(matrix1, demons1, d_costs1, buffer_size1)
-#     sol1 = gb_solver.solve(task1)
-#     ic(sol1)
-# #     # solution_print(task1, sol1)
-# #     # mat_print(matrix1, sol1[0])
-# #     if verify_solution(task1, sol1):
-# #         solution_print(task1, sol1)
-# #
-# #     else:
-# #         print("AAAAAAAAAAAAAAAAAA")
