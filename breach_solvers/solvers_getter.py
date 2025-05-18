97034f3bc6587543ff2ae960eade139c02537f3d
283018 <283018@student.pwr.edu.pl>
2025-05-18 03:43:26 +0200

    Restructurization breach_solvers module

---
+++ breach_solvers/solvers_getter.py
@@ -0,0 +1,31 @@
+from typing import overload, Literal
+
+from .solvers import BruteSolver, GurobiSolver, AntColSolver
+from .solvers_protocol import Solver, solver_registry
+
+
+@overload
+def get_solver(name: Literal['gurobi']) -> GurobiSolver:
+    ...
+
+@overload
+def get_solver(name: Literal['brute']) -> BruteSolver:
+    ...
+
+@overload
+def get_solver(name: Literal['ant_col']) -> AntColSolver:
+    ...
+
+
+# TODO: docstring kwargs update
+def get_solver(name: str) -> Solver:
+    """
+    Get solver instance by name:
+
+    :param name: {'gurobi', 'brute', 'ant_col'}
+    :return: solver instance
+    """
+    solver_class = solver_registry.get(name)
+    if not solver_class:
+        raise ValueError(f"Unknown solver: {name}, must be one of {list(solver_registry.keys())}")
+    return solver_class()
